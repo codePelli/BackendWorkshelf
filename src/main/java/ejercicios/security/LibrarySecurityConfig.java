be9f6b67e811057678cd19966c1530758777e680
carlavelasco15 <carlavelasco15@gmail.com>
2023-12-10 12:45:07 +0100

    Tests CORS

--- src/main/java/ejercicios/security/LibrarySecurityConfig.java
+++ src/main/java/ejercicios/security/LibrarySecurityConfig.java
@@ -14,6 +14,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import ejercicios.jwt.JWTAuthenticationFilter;
@@ -22,6 +29,7 @@ import ejercicios.jwt.JWTAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class LibrarySecurityConfig {
 	@Autowired
 	private JWTAuthenticationFilter authenticationFilter;
@@ -57,22 +65,40 @@ public class LibrarySecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().and().cors().disable()
-                .authorizeHttpRequests()
-                .requestMatchers(UN_SECURED_URLs).permitAll().and()
-                .authorizeHttpRequests().requestMatchers(SECURED_URLs)
-                .hasAuthority("ADMIN").anyRequest().authenticated()
-                .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider())
-                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
-    }
+	SecurityFilterChain fliterChain (HttpSecurity httpSecurity) throws Exception {
+		
+		return httpSecurity
+				.csrf(csrf -> csrf.disable())
+				.cors( cors -> corsConfigurationSource())
+				.authorizeHttpRequests( auth -> auth
+				.requestMatchers("/api/v1/visit/{id}/{name}").access(new WebExpressionAuthorizationManager("#name == authentication.name"))
+				.requestMatchers("/swagger-ui/**","/doc.html").permitAll()
+				.anyRequest().authenticated()
+				)
+				.formLogin( e -> e.permitAll()
+						.successHandler(swaggerRedirect()))		
+				.build();
+	} 
+	
+	// CORS Configuration Bean
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
+   CorsConfigurationSource corsConfigurationSource() {
+    	
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");      // Allow all origins or Arrays.asList("http://localhost:4200","http://localhost:3000")
+        configuration.addAllowedMethod("*");      // Allow all methods or List.of("GET", "POST", "PUT", "DELETE")
+        configuration.addAllowedHeader("*");      // Allow all headers
+        configuration.setAllowCredentials(true);  // Allow sending of authentication cookies
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
+
+    public AuthenticationSuccessHandler swaggerRedirect(){
+        return ((request, response, authentication) -> {
+            response.sendRedirect("/doc.html");
+        });
     }
 
 }

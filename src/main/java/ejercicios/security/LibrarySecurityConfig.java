4da50a1d7d7e72661802b4403bcfca19d2767985
Marc Pellicer <marcpelli3@gmail.com>
2023-12-10 13:11:09 +0100

    test 5

--- src/main/java/ejercicios/security/LibrarySecurityConfig.java
+++ src/main/java/ejercicios/security/LibrarySecurityConfig.java
@@ -4,7 +4,6 @@ package ejercicios.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;

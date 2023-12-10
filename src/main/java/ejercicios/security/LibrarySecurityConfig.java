0b2157483b7b25244c1d951c1bf550c6a1f7283c
Marc Pellicer <marcpelli3@gmail.com>
2023-12-10 15:40:29 +0100

    removed

--- src/main/java/ejercicios/security/LibrarySecurityConfig.java
+++ src/main/java/ejercicios/security/LibrarySecurityConfig.java
@@ -85,7 +85,6 @@ public class LibrarySecurityConfig {
     	
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("*");  // Allow all origins or Arrays.asList("http://localhost:4200","http://localhost:3000")
-        configuration.addAllowedOriginPattern("*");
         configuration.addAllowedMethod("*");      // Allow all methods or List.of("GET", "POST", "PUT", "DELETE")
         configuration.addAllowedHeader("*");      // Allow all headers
         configuration.setAllowCredentials(true);  // Allow sending of authentication cookies

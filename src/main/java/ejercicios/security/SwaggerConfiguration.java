ac8eee396da772aa2f028603844df318cb01597a
sergial273 <97388630+sergial273@users.noreply.github.com>
2023-12-14 12:52:50 +0100

    testing swagger

--- src/main/java/ejercicios/security/SwaggerConfiguration.java
+++ src/main/java/ejercicios/security/SwaggerConfiguration.java
@@ -2,23 +2,30 @@ package ejercicios.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
+@Configuration
 public class SwaggerConfiguration {
-	@Configuration
-	@EnableSwagger2
-	public class SwaggerConfig {
-	@Bean
-		public Docket api() {
-			return new Docket(DocumentationType.SWAGGER_2)
-						.select()
-						.apis(RequestHandlerSelectors.any())
-						.paths(PathSelectors.any())
-						.build();
-		}
-	}
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+            .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+            .info(new Info().title("WorkShelf")
+                .description("description of API.")
+                .version("1.0")
+                .license(new License().name("License of API")
+                    .url("API license URL")));
+    }
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+            .bearerFormat("JWT")
+            .scheme("bearer")
+            .description("Authorize Client");
+    }
 }
